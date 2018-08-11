70e52eccef39f1ae628ae0c019362f88387ea757
1378789620@qq.com <1378789620@qq.com>
2018-08-11 18:32:18 +0800

    完成管理员商品添加的类别树回显

--- e3-manager-web/src/main/java/com/iteason/controller/ItemController.java
+++ e3-manager-web/src/main/java/com/iteason/controller/ItemController.java
@@ -1,12 +1,16 @@
 package com.iteason.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.iteason.intef.ItemService;
 import com.iteason.pojo.EasyUIDatagridResult;
+import com.iteason.pojo.EsayUIZtreeNode;
 
 @Controller
 public class ItemController {
@@ -31,5 +35,21 @@ public class ItemController {
 		
 		return result;
 		
+	}
+	
+	/**
+	 * 
+	 * @author 阿荣
+	 * @Description:通过cid作为父ip，查找类别的tree
+	 * @date: 2018年8月11日 下午2:57:49
+	 * @param parentId
+	 * @return
+	 */
+	@RequestMapping(value="/item/cat/list")
+	@ResponseBody
+	public List<EsayUIZtreeNode> findCatZtree(@RequestParam(name="id",defaultValue="0")
+			Long parentId){
+		List<EsayUIZtreeNode> list  = 	itemService.findCatZtree(parentId);
+		return list;
 	}
 }
--- e3-manager/e3-manager-service/src/main/java/com/iteason/service/ItemServiceImp.java
+++ e3-manager/e3-manager-service/src/main/java/com/iteason/service/ItemServiceImp.java
@@ -1,31 +1,42 @@
 package com.iteason.service;
 
+import java.util.ArrayList;
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.iteason.intef.ItemService;
+import com.iteason.mapper.TbItemCatMapper;
 import com.iteason.mapper.TbItemMapper;
 import com.iteason.pojo.EasyUIDatagridResult;
+import com.iteason.pojo.EsayUIZtreeNode;
 import com.iteason.pojo.TbItem;
+import com.iteason.pojo.TbItemCat;
+import com.iteason.pojo.TbItemCatExample;
 import com.iteason.pojo.TbItemExample;
+import com.iteason.pojo.TbItemExample.Criteria;
 @Service
 public class ItemServiceImp implements ItemService {
-	private EasyUIDatagridResult result = new  EasyUIDatagridResult();
-	@Autowired
-	private TbItemMapper mapper;
 	
+	
+	
+	@Autowired
+	private TbItemMapper tbItemMapper;
+	
+	@Autowired
+	private TbItemCatMapper tbItemCatMapper;
 	@Override
 	public EasyUIDatagridResult findItemList(int page, int rows) {
+		EasyUIDatagridResult result = new  EasyUIDatagridResult();
 		// 使用mybatis分页插件，查询相应item数据
 		PageHelper.startPage(page, rows);
 		
 		TbItemExample example = new TbItemExample();
-		List<TbItem> list = mapper.selectByExample(example);
+		List<TbItem> list = tbItemMapper.selectByExample(example);
 		//封装成PageInfo对象
 		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
 		//封装总条数
@@ -33,6 +44,27 @@ public class ItemServiceImp implements ItemService {
 		//封装数据
 		result.setRows(list);
 		return result;
+	}
+	
+	@Override
+	public List<EsayUIZtreeNode> findCatZtree(Long parentId) {
+		 List<EsayUIZtreeNode> nodeList = new ArrayList<EsayUIZtreeNode>();
+		TbItemCatExample example = new TbItemCatExample();
+		//添加条件
+		com.iteason.pojo.TbItemCatExample.Criteria c = example.createCriteria();
+		c.andParentIdEqualTo(parentId);
+		//  通过cid作为父ip，查找类别的tree
+		List<TbItemCat> list = tbItemCatMapper.selectByExample(example);
+		//封装成EsayUIZtreeNode对象
+		for(TbItemCat tbItemCat:list){
+			System.out.println(tbItemCat.getId()+"---------------------");
+			EsayUIZtreeNode tree = new EsayUIZtreeNode();
+			tree.setId(tbItemCat.getId());
+			tree.setText(tbItemCat.getName());
+			tree.setState(tbItemCat.getIsParent()?"closed":"open");
+			nodeList.add(tree);
+		}
+		return nodeList;
 	}
 	
 	
