2c75344e4ea4000886a729aeb009d092340676a9
1378789620@qq.com <1378789620@qq.com>
2018-08-21 19:23:25 +0800

    修正jedis工具类相关数据

--- e3-common/src/main/java/com/iteason/jedis/JedisClient.java
+++ e3-common/src/main/java/com/iteason/jedis/JedisClient.java
@@ -2,13 +2,14 @@ package com.iteason.jedis;
 
 public interface JedisClient {
 
-	String set(String key, String value);
-	String get(String key);
-	Boolean exists(String key);
-	Long expire(String key, int seconds);
-	Long ttl(String key);
-	Long incr(String key);
-	Long hset(String key, String field, String value);
-	String hget(String key, String field);
-	Long hdel(String key, String... field);
+	String set(String key, String value);//插入数据
+	String get(String key);//读取数据
+	Long del(String key);//删除string类型数据
+	Boolean exists(String key);//判断存在
+	Long expire(String key, int seconds);//设置过期时间
+	Long ttl(String key);//查询过期时间
+	Long incr(String key);//自增
+	Long hset(String key, String field, String value);//插入hash类型数据
+	String hget(String key, String field);//获得hash类型数据
+	Long hdel(String key, String... field);//删除hash数据
 }
