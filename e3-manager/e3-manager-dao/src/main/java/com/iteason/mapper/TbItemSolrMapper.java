367c52ef6c5d02234c84e9e48a98fc04e918b2a8
1378789620@qq.com <1378789620@qq.com>
2018-08-23 23:14:36 +0800

    完成solr索引文档的批导入功能

---
+++ e3-manager/e3-manager-dao/src/main/java/com/iteason/mapper/TbItemSolrMapper.java
@@ -0,0 +1,9 @@
+package com.iteason.mapper;
+
+import java.util.List;
+
+import com.iteason.pojo.TbItemSolrResult;
+
+public interface TbItemSolrMapper {
+	List<TbItemSolrResult> selectTbItemSolrResultList();
+}
---
+++ e3-manager/e3-manager-interface/src/main/java/com/iteason/intef/SolrDocumentService.java
@@ -0,0 +1,7 @@
+package com.iteason.intef;
+
+public interface SolrDocumentService {
+
+	void addDocument() throws Exception;
+
+}
